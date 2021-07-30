6c5220b8accd980d9ab3292fef09ca182c16eee8
jiangze.he <jiangze.he@picovr.com>
2021-07-30 16:43:00 +0800

    nacos config

--- cloud-gateway/src/main/java/com/dukoia/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/dukoia/cloud/gateway/CloudGatewayApplication.java
@@ -2,7 +2,12 @@ package com.dukoia.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 
 /**
@@ -13,10 +18,25 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
     }
 
+    @Bean
+    public ServiceInstanceListSupplier discoveryClientServiceInstanceListSupplier(
+            ConfigurableApplicationContext context) {
+        return ServiceInstanceListSupplier.builder()
+                .withDiscoveryClient()
+                .withHealthChecks()
+                .build(context);
+    }
+
+//    @Bean
+//    public CaffeineCacheManager caffeineCacheManager(){
+//        return new CaffeineCacheManager();
+//    }
 }
+
